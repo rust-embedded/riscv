a51143d366eedfe5ab30bfdf5f1f491ce230f549
Vadim Kaushan <admin@disasm.info>
2018-12-23 11:25:04 +0100

    Implement asm functions

--- build.rs
+++ build.rs
@@ -1,7 +1,20 @@
-use std::env;
+use std::path::PathBuf;
+use std::{env, fs};
 
 fn main() {
     let target = env::var("TARGET").unwrap();
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let name = env::var("CARGO_PKG_NAME").unwrap();
+
+    if target.starts_with("riscv") && env::var_os("CARGO_FEATURE_INLINE_ASM").is_none() {
+        fs::copy(
+            format!("bin/{}.a", target),
+            out_dir.join(format!("lib{}.a", name)),
+        ).unwrap();
+
+        println!("cargo:rustc-link-lib=static={}", name);
+        println!("cargo:rustc-link-search={}", out_dir.display());
+    }
 
     if target.starts_with("riscv32") {
         println!("cargo:rustc-cfg=riscv");
