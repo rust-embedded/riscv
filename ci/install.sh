a8040bd24bd705f515852767a4d640cbb643b41c
Vadim Kaushan <admin@disasm.info>
2019-03-17 16:45:28 +0300

    Check blobs in separate target

--- ci/install.sh
+++ ci/install.sh
@@ -2,7 +2,11 @@
 
 set -euxo pipefail
 
-rustup target add $TARGET
+if [ -n "${TARGET:-}" ]; then
+    rustup target add $TARGET
+fi
 
-mkdir gcc
-curl -L https://static.dev.sifive.com/dev-tools/riscv64-unknown-elf-gcc-8.1.0-2018.12.0-x86_64-linux-ubuntu14.tar.gz | tar --strip-components=1 -C gcc -xz
+if [ -n "${CHECK_BLOBS:-}" ]; then
+    mkdir gcc
+    curl -L https://static.dev.sifive.com/dev-tools/riscv64-unknown-elf-gcc-8.1.0-2018.12.0-x86_64-linux-ubuntu14.tar.gz | tar --strip-components=1 -C gcc -xz
+fi
--- ci/script.sh
+++ ci/script.sh
@@ -2,12 +2,14 @@
 
 set -euxo pipefail
 
-cargo check --target $TARGET
+if [ -n "${TARGET:-}" ]; then
+    cargo check --target $TARGET
 
-if [ $TRAVIS_RUST_VERSION = nightly ]; then
-    cargo check --target $TARGET --features inline-asm
+    if [ $TRAVIS_RUST_VERSION = nightly ]; then
+        cargo check --target $TARGET --features inline-asm
+    fi
 fi
 
-if [ $TARGET = x86_64-unknown-linux-gnu ]; then
+if [ -n "${CHECK_BLOBS:-}" ]; then
     ./check-blobs.sh
 fi
