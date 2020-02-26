b729dbfb09e353174d270434c11625ba08d7009f
Vadim Kaushan <admin@disasm.info>
2020-02-26 22:40:53 +0300

    Add support for riscv32i

--- riscv-rt/assemble.sh
+++ riscv-rt/assemble.sh
@@ -11,6 +11,9 @@ riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32imac asm.S -o bin/$crate.o
 ar crs bin/riscv32imac-unknown-none-elf.a bin/$crate.o
 ar crs bin/riscv32imc-unknown-none-elf.a bin/$crate.o
 
+riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32i asm.S -o bin/$crate.o
+ar crs bin/riscv32i-unknown-none-elf.a bin/$crate.o
+
 riscv64-unknown-elf-gcc -c -mabi=lp64 -march=rv64imac asm.S -o bin/$crate.o
 ar crs bin/riscv64imac-unknown-none-elf.a bin/$crate.o
 ar crs bin/riscv64gc-unknown-none-elf.a bin/$crate.o
