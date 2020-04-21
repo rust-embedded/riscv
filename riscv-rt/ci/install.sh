642ef2944e431e2a40b14fdcf1de8d19f29c15a6
Vadim Kaushan <admin@disasm.info>
2020-04-22 00:05:17 +0300

    Update CI scripts

--- riscv-rt/ci/install.sh
+++ riscv-rt/ci/install.sh
@@ -1,12 +1,10 @@
+#!/usr/bin/env bash
+
 set -euxo pipefail
 
-main() {
-    if [ $TARGET != x86_64-unknown-linux-gnu ]; then
-        rustup target add $TARGET
-    fi
+if [ $TARGET != x86_64-unknown-linux-gnu ]; then
+    rustup target add $TARGET
+fi
 
-    mkdir gcc
-    curl -L https://static.dev.sifive.com/dev-tools/riscv64-unknown-elf-gcc-8.1.0-2018.12.0-x86_64-linux-ubuntu14.tar.gz | tar --strip-components=1 -C gcc -xz
-}
-
-main
+mkdir gcc
+curl -L https://static.dev.sifive.com/dev-tools/riscv64-unknown-elf-gcc-8.1.0-2018.12.0-x86_64-linux-ubuntu14.tar.gz | tar --strip-components=1 -C gcc -xz
--- riscv-rt/ci/script.sh
+++ riscv-rt/ci/script.sh
@@ -1,18 +1,17 @@
+#!/usr/bin/env bash
+
 set -euxo pipefail
 
-main() {
-    cargo check --target $TARGET
-    if [[ $TARGET == riscv* ]]; then
-        cargo check --target $TARGET --examples
-    fi
+cargo check --target $TARGET
+if [[ $TARGET == riscv* ]]; then
+    cargo check --target $TARGET --examples
+fi
 
-    if [ $TRAVIS_RUST_VERSION = nightly ]; then
-        cargo check --target $TARGET --features 'inline-asm'
-    fi
+if [ $TRAVIS_RUST_VERSION = nightly ]; then
+    cargo check --target $TARGET --features 'inline-asm'
+fi
 
-    if [ $TARGET = x86_64-unknown-linux-gnu ]; then
-        ./check-blobs.sh
-    fi
-}
-
-main
+if [ $TARGET = x86_64-unknown-linux-gnu ]; then
+    PATH="$PATH:$PWD/gcc/bin"
+    ./check-blobs.sh
+fi
