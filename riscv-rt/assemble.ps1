12fb268f1281a3e4d5b1f874ad276cc33ea54c43
Ilya Epifanov <elijah.epifanov@gmail.com>
2020-05-15 23:14:15 +0200

    fixed the assemble.ps1 PowerShell™ script

--- riscv-rt/assemble.ps1
+++ riscv-rt/assemble.ps1
@@ -3,12 +3,13 @@ Remove-Item -Force bin/*.a
 
 $crate = "riscv-rt"
 $extension_sets = @("i", "im", "ic", "imc")
+$pwd = Get-Location
 
 foreach ($ext in $extension_sets) {
-    riscv64-unknown-elf-gcc -ggdb3 -c -mabi=ilp32 -march=rv32$ext asm.S -o bin/$crate.o
+    riscv64-unknown-elf-gcc -ggdb3 -fdebug-prefix-map=$pwd=/riscv-rt -c -mabi=ilp32 -march=rv32$ext asm.S -o bin/$crate.o
     riscv64-unknown-elf-ar crs bin/riscv32$ext-unknown-none-elf.a bin/$crate.o
 
-    riscv64-unknown-elf-gcc -ggdb3 -c -mabi=lp64 -march=rv64$ext asm.S -o bin/$crate.o
+    riscv64-unknown-elf-gcc -ggdb3 -fdebug-prefix-map=$pwd=/riscv-rt -c -mabi=lp64 -march=rv64$ext asm.S -o bin/$crate.o
     riscv64-unknown-elf-ar crs bin/riscv64$ext-unknown-none-elf.a bin/$crate.o
 }
 
