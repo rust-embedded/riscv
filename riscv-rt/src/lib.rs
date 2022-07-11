dd232177887f1dc38587a83e1d6cb86a280cfa4e
Vadim Kaushan <admin@disasm.info>
2022-07-11 11:19:29 +0300

    Simplify build.rs example

--- riscv-rt/src/lib.rs
+++ riscv-rt/src/lib.rs
@@ -81,23 +81,17 @@
 //!
 //! ``` ignore,no_run
 //! use std::env;
-//! use std::fs::File;
-//! use std::io::Write;
-//! use std::path::Path;
+//! use std::fs;
+//! use std::path::PathBuf;
 //!
-//! /// Put the linker script somewhere the linker can find it.
 //! fn main() {
-//!     let out_dir = env::var("OUT_DIR").expect("No out dir");
-//!     let dest_path = Path::new(&out_dir);
-//!     let mut f = File::create(&dest_path.join("memory.x"))
-//!         .expect("Could not create file");
+//!     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
 //!
-//!     f.write_all(include_bytes!("memory.x"))
-//!         .expect("Could not write file");
-//!
-//!     println!("cargo:rustc-link-search={}", dest_path.display());
-//!
+//!     // Put the linker script somewhere the linker can find it.
+//!     fs::write(out_dir.join("memory.x"), include_bytes!("memory.x")).unwrap();
+//!     println!("cargo:rustc-link-search={}", out_dir.display());
 //!     println!("cargo:rerun-if-changed=memory.x");
+//!
 //!     println!("cargo:rerun-if-changed=build.rs");
 //! }
 //! ```
