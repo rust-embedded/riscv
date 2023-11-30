b74b90bd5840ed1cba2a47f819c588d7d9dce36a
Aaron Gowatch <agowatch@amazon.com>
2023-11-29 21:57:32 -0700

    Update variable name

--- riscv/src/delay.rs
+++ riscv/src/delay.rs
@@ -28,8 +28,8 @@ impl DelayNs for McycleDelay {
     #[inline]
    fn delay_ns(&mut self, ns: u32) {
         let t0 = mcycle::read64();
-        let us_64: u64 = ns.into();
-        let clock = (us_64 * (self.ticks_second as u64)) / 1_000_000_000u64;
+        let ns_64: u64 = ns.into();
+        let clock = (ns_64 * (self.ticks_second as u64)) / 1_000_000_000u64;
         while mcycle::read64().wrapping_sub(t0) <= clock {}
    }
 }
