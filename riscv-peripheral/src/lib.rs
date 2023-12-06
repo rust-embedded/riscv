51066b53b9e94dd32546e2da4d4074e7c521ea73
Román Cárdenas <rcardenas.rod@gmail.com>
2023-12-06 19:04:57 +0100

    prepare for riscv

--- riscv-peripheral/src/lib.rs
+++ riscv-peripheral/src/lib.rs
@@ -7,8 +7,8 @@ pub use riscv; // re-export riscv crate to allow macros to use it
 
 pub mod common; // common definitions for all peripherals
 pub mod hal; // trait implementations for embedded-hal
-#[cfg(feature = "hal-async")]
-pub mod hal_async; // async trait implementations for embedded-hal
+             // #[cfg(feature = "hal-async")]
+             // pub mod hal_async; // async trait implementations for embedded-hal
 pub mod macros; // macros for easing the definition of peripherals in PACs
 
 pub mod aclint; // ACLINT and CLINT peripherals
