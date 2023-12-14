82de9216f7a12a8542c02264bc18318bfa7802a8
Román Cárdenas <rcardenas.rod@gmail.com>
2023-12-14 14:20:11 +0100

    store waker

--- riscv-peripheral/src/hal_async/aclint.rs
+++ riscv-peripheral/src/hal_async/aclint.rs
@@ -2,23 +2,29 @@
 
 use crate::aclint::mtimer::MTIME;
 pub use crate::hal::aclint::Delay;
-pub use crate::hal_async::delay::DelayUs;
+pub use crate::hal_async::delay::DelayNs;
 use core::{
     future::Future,
     pin::Pin,
-    task::{Context, Poll},
+    task::{Context, Poll, Waker},
 };
 
 struct DelayAsync {
     mtime: MTIME,
     t0: u64,
     n_ticks: u64,
+    waker: Option<Waker>,
 }
 
 impl DelayAsync {
     pub fn new(mtime: MTIME, n_ticks: u64) -> Self {
         let t0 = mtime.read();
-        Self { mtime, t0, n_ticks }
+        Self {
+            mtime,
+            t0,
+            n_ticks,
+            waker: None,
+        }
     }
 }
 
@@ -26,15 +32,32 @@ impl Future for DelayAsync {
     type Output = ();
 
     #[inline]
-    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
         match self.mtime.read().wrapping_sub(self.t0) < self.n_ticks {
-            true => Poll::Pending,
-            false => Poll::Ready(()),
+            true => {
+                self.get_mut().waker = Some(cx.waker().clone());
+                Poll::Pending
+            }
+            false => {
+                if let Some(waker) = self.get_mut().waker.take() {
+                    waker.wake();
+                } else {
+                    // corner case: delay expired before polling for the first time
+                    cx.waker().wake_by_ref();
+                };
+                Poll::Ready(())
+            }
         }
     }
 }
 
-impl DelayUs for Delay {
+impl DelayNs for Delay {
+    #[inline]
+    async fn delay_ns(&mut self, ns: u32) {
+        let n_ticks = ns as u64 * self.get_freq() as u64 / 1_000_000_000;
+        DelayAsync::new(self.get_mtime(), n_ticks).await;
+    }
+
     #[inline]
     async fn delay_us(&mut self, us: u32) {
         let n_ticks = us as u64 * self.get_freq() as u64 / 1_000_000;
