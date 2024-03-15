70ac4aa31355ef55c0cdbbff1d3bcf5d94c32ffc
Henri Lunnikivi <henri.lunnikivi@tuni.fi>
2024-03-15 12:16:38 +0200

    Impl priority, level regs (WIP)
    - Using 32-bit writes to workaround a possible HW bug

--- riscv-peripheral/src/clic/intctl.rs
+++ riscv-peripheral/src/clic/intctl.rs
@@ -1,6 +1,18 @@
 //! CLIC interrupt control register.
 
+use crate::common::{Reg, RW};
+
 /// CLIC interrupt control register.
+///
+/// A configurable number of upper bits in clicintctl[i] are assigned to encode the interrupt
+/// level.
+///
+/// The least-significant bits in clicintctl[i] that are not configured to be part of the
+/// interrupt level are interrupt priority, which are used to prioritize among interrupts
+/// pending-and-enabled at the same privilege mode and interrupt level. The highest-priority
+/// interrupt at a given privilege mode and interrupt level is taken first. In case there are
+/// multiple pending-and-enabled interrupts at the same highest priority, the highest-numbered
+/// interrupt is taken first.
 #[derive(Clone, Copy, Debug, Eq, PartialEq)]
 #[repr(transparent)]
 pub struct INTCTL {
@@ -8,6 +20,8 @@ pub struct INTCTL {
 }
 
 impl INTCTL {
+    const INTCTL_OFFSET: usize = 0x3;
+
     /// Creates a new interrupt control register from a base address.
     ///
     /// # Safety
@@ -16,5 +30,57 @@ impl INTCTL {
     #[inline]
     pub(crate) const unsafe fn new(address: usize) -> Self {
         Self { ptr: address as _ }
+    }
+
+    /// Check interrupt level for this interrupt.
+    #[inline]
+    pub fn level(self) -> u8 {
+        // SAFETY: valid interrupt number
+        let reg: Reg<u32, RW> = unsafe { Reg::new(self.ptr) };
+
+        // TODO: need to figure out how many are actually priority bits and how many are level bits
+        // and mask accordingly
+        reg.read_bits(8 * Self::INTCTL_OFFSET, 7 + 8 * Self::INTCTL_OFFSET) as u8
+    }
+
+    /// Set interrupt level for this interrupt.
+    #[inline]
+    pub fn set_level(self, level: u8) {
+        // SAFETY: valid interrupt number
+        let reg: Reg<u32, RW> = unsafe { Reg::new(self.ptr) };
+
+        // TODO: need to figure out how many are actually priority bits and how many are level bits
+        // and mask accordingly
+        reg.write_bits(
+            8 * Self::INTCTL_OFFSET,
+            7 + 8 * Self::INTCTL_OFFSET,
+            level as _,
+        )
+    }
+
+    /// Check interrupt priority for this interrupt.
+    ///
+    /// N.b., 2024-03-11 rt-ss/CLIC does not implement priority bits at all at this time
+    #[inline]
+    pub fn priority(self) -> u32 {
+        // SAFETY: valid interrupt number
+        let reg: Reg<u32, RW> = unsafe { Reg::new(self.ptr) };
+
+        // TODO: need to figure out how many are actually priority bits and how many are level bits
+        // and mask accordingly
+        reg.read()
+    }
+
+    /// Set interrupt priority for this interrupt.
+    ///
+    /// N.b., 2024-03-11 rt-ss/CLIC does not implement priority bits at all at this time
+    #[inline]
+    pub fn set_priority(self, priority: u32) {
+        // SAFETY: valid interrupt number
+        let reg: Reg<u32, RW> = unsafe { Reg::new(self.ptr) };
+
+        // TODO: need to figure out how many are actually priority bits and how many are level bits
+        // and mask accordingly
+        reg.write(priority)
     }
 }
