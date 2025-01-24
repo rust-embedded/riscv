3e499c0f66c8617e7a9bad6eeaa90c2389a453d1
ia0 <git@ia0.eu>
2025-01-24 13:31:16 +0100

    Add RISCV_MTVEC_ALIGN to control the alignment of the vector table
    RISC-V requires a minimum of 4 bytes, but known implementations require 64 or even 256 bytes.

--- riscv-rt/build.rs
+++ riscv-rt/build.rs
@@ -47,6 +47,13 @@ fn main() {
         // make sure that these env variables are not changed without notice.
         println!("cargo:rerun-if-env-changed=RISCV_RT_BASE_ISA");
         println!("cargo:rerun-if-env-changed=RISCV_RT_LLVM_ARCH_PATCH");
+        if env::var_os("CARGO_FEATURE_V_TRAP").is_some()
+            && env::var_os("CARGO_FEATURE_NO_INTERRUPTS").is_none()
+        {
+            // This environment variable is used by the `#[riscv::pac_enum()]` call in
+            // `src/interrupts.rs` (when `v-trap` is enabled and `no-interrupts` disabled).
+            println!("cargo:rerun-if-env-changed=RISCV_MTVEC_ALIGN");
+        }
 
         for flag in target.rustc_flags() {
             // Required until target_feature risc-v is stable and in-use
