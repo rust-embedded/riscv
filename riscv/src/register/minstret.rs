75c0048111517e874c2a66883e8309cdce6dab04
Henri Lunnikivi <heggggxa@gmail.com>
2025-02-20 17:13:33 +0200

    riscv: simplify macro call in riscv/src/register/minstret.rs
    Co-authored-by: Román Cárdenas Rodríguez <rcardenas.rod@gmail.com>

--- riscv/src/register/minstret.rs
+++ riscv/src/register/minstret.rs
@@ -3,4 +3,4 @@
 read_csr_as_usize!(0xB02);
 write_csr_as_usize!(0xB02);
 read_composite_csr!(super::minstreth::read(), read());
-write_composite_csr!(|bits| super::minstreth::write(bits), |bits| write(bits));
+write_composite_csr!(super::minstreth::write, write);
