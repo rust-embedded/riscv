abb6f338e1a10cae6868c5026794edb54c460c43
Henri Lunnikivi <heggggxa@gmail.com>
2025-02-20 17:13:21 +0200

    riscv: simplify macro call in riscv/src/register/mcycle.rs
    Co-authored-by: Román Cárdenas Rodríguez <rcardenas.rod@gmail.com>

--- riscv/src/register/mcycle.rs
+++ riscv/src/register/mcycle.rs
@@ -3,4 +3,4 @@
 read_csr_as_usize!(0xB00);
 write_csr_as_usize!(0xB00);
 read_composite_csr!(super::mcycleh::read(), read());
-write_composite_csr!(|bits| super::mcycleh::write(bits), |bits| write(bits));
+write_composite_csr!(super::mcycleh::write, write);
