bfe30b22ce01cf649320fd6eb186408389dd9904
rmsyn <github@weathered-steel.dev>
2025-05-18 02:32:24 +0000

    tests: add `mconfigptr` unit tests
    Adds basic unit tests for the `mconfigptr` CSR.

--- riscv/src/register/mconfigptr.rs
+++ riscv/src/register/mconfigptr.rs
@@ -52,3 +52,56 @@ impl Mconfigptr {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mconfigptr() {
+        #[cfg(target_arch = "riscv32")]
+        const EXP_SHIFT: usize = 2;
+        #[cfg(not(target_arch = "riscv32"))]
+        const EXP_SHIFT: usize = 3;
+
+        const EXP_MASK: usize = (1usize << EXP_SHIFT) - 1;
+
+        assert_eq!(Mconfigptr::ALIGN_SHIFT, EXP_SHIFT);
+        assert_eq!(Mconfigptr::ALIGN_MASK, EXP_MASK);
+
+        (1..usize::BITS)
+            .map(|b| ((1u128 << b) - 1) as usize)
+            .for_each(|ptr| {
+                let mconfigptr = Mconfigptr::from_bits(ptr);
+                assert_eq!(mconfigptr.bits(), ptr);
+
+                match mconfigptr.try_as_ptr() {
+                    Ok(cfg_ptr) => {
+                        assert_eq!(cfg_ptr, ptr as *const _);
+                        assert_eq!(mconfigptr.as_ptr(), ptr as *const _);
+                    }
+                    Err(err) if ptr == 0 => assert_eq!(
+                        err,
+                        Error::InvalidFieldVariant {
+                            field: "mconfigptr",
+                            value: 0
+                        }
+                    ),
+                    Err(err) => assert_eq!(
+                        err,
+                        Error::InvalidFieldValue {
+                            field: "mconfigptr",
+                            value: ptr,
+                            bitmask: !Mconfigptr::ALIGN_MASK,
+                        }
+                    ),
+                }
+
+                let aligned_ptr = ptr << Mconfigptr::ALIGN_SHIFT;
+                let aligned_mconfigptr = Mconfigptr::from_bits(aligned_ptr);
+
+                assert_eq!(aligned_mconfigptr.try_as_ptr(), Ok(aligned_ptr as *const _));
+                assert_eq!(aligned_mconfigptr.as_ptr(), aligned_ptr as *const _);
+            });
+    }
+}
