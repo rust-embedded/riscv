f41190ebc235b15092927d52ebee19b6c9a0327c
KushalMeghani1644 <kushalmeghani108@gmail.com>
2025-11-10 16:42:04 +0530

    Fix formatting

--- riscv/src/register/dpc.rs
+++ riscv/src/register/dpc.rs
@@ -18,12 +18,14 @@ mod tests {
 
     #[test]
     fn test_dpc_bits_roundtrip() {
-        (0..=usize::BITS).map(|r| ((1u128 << r) - 1) as usize).for_each(|pc| {
-            // ensure lowest bit is cleared
-            let exp_pc = pc & !1usize;
-            let dpc = Dpc::from_bits(pc);
-            assert_eq!(dpc.bits(), exp_pc);
-            assert_eq!(Dpc::from_bits(dpc.bits()).bits(), dpc.bits());
-        });
+        (0..=usize::BITS)
+            .map(|r| ((1u128 << r) - 1) as usize)
+            .for_each(|pc| {
+                // ensure lowest bit is cleared
+                let exp_pc = pc & !1usize;
+                let dpc = Dpc::from_bits(pc);
+                assert_eq!(dpc.bits(), exp_pc);
+                assert_eq!(Dpc::from_bits(dpc.bits()).bits(), dpc.bits());
+            });
     }
 }
