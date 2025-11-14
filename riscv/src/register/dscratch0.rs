c55b3cc89b9f721f1cc1ed76e490403298eee9f9
KushalMeghani1644 <kushalmeghani108@gmail.com>
2025-11-14 15:00:27 +0530

    Fix errors

--- riscv/src/register/dscratch0.rs
+++ riscv/src/register/dscratch0.rs
@@ -1,21 +1,17 @@
 //! dscratch0
 
-read_write_csr_as_usize!(Dscratch0, 0x7b2);
+read_write_csr_as_usize!(0x7b2);
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_dscratch0_mask() {
-        let reg = Dscratch0::from_bits(usize::MAX);
-        assert_eq!(reg.bits(), usize::MAX);
-        assert_eq!(Dscratch0::BITMASK, usize::MAX);
-    }
-
-    #[test]
-    fn test_dscratch0_roundtrip() {
-        let reg = Dscratch0::from_bits(0xDEAD_BEEFusize);
-        assert_eq!(reg.bits(), 0xDEAD_BEEFusize);
+    fn test_dscratch0_read_write() {
+        for i in 0..usize::BITS {
+            let val = 1usize << i;
+            let _ = unsafe { try_write(val) };
+            let _ = try_read();
+        }
     }
 }
--- riscv/src/register/dscratch1.rs
+++ riscv/src/register/dscratch1.rs
@@ -1,21 +1,17 @@
 //! dscratch1
 
-read_write_csr_as_usize!(Dscratch1, 0x7b3);
+read_write_csr_as_usize!(0x7b3);
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_dscratch1_mask() {
-        let reg = Dscratch1::from_bits(usize::MAX);
-        assert_eq!(reg.bits(), usize::MAX);
-        assert_eq!(Dscratch1::BITMASK, usize::MAX);
-    }
-
-    #[test]
-    fn test_dscratch1_roundtrip() {
-        let reg = Dscratch1::from_bits(0xDEAD_BEEFusize);
-        assert_eq!(reg.bits(), 0xDEAD_BEEFusize);
+    fn test_dscratch1_read_write() {
+        for i in 0..usize::BITS {
+            let val = 1usize << i;
+            let _ = unsafe { try_write(val) };
+            let _ = try_read();
+        }
     }
 }
