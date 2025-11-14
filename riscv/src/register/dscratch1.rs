7c6934a3fcff323d354754f58079a1cc8daddce7
KushalMeghani1644 <kushalmeghani108@gmail.com>
2025-11-14 21:23:41 +0530

    Update tests

--- riscv/src/register/dscratch1.rs
+++ riscv/src/register/dscratch1.rs
@@ -5,13 +5,14 @@ read_write_csr_as_usize!(0x7b3);
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::result::Error;
 
     #[test]
     fn test_dscratch1_read_write() {
         for i in 0..usize::BITS {
             let val = 1usize << i;
-            let _ = unsafe { try_write(val) };
-            let _ = try_read();
+            assert_eq!(unsafe { try_write(val) }, Err(Error::Unimplemented));
+            assert_eq!(try_read(), Err(Error::Unimplemented));
         }
     }
 }
